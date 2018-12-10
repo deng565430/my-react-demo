024f73d038ade63ef61ff85b143eca3d62c72afd
deng565430 <ds@dsdeMacBook-Pro.local>
2018-12-10 12:04:21 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import * as React from 'react'
 import { Button } from 'antd'
 import './assets/index.css'
 
+console.log(34344545656)
+
 const img = require('./test.jpeg')
 
 class App extends React.Component {
